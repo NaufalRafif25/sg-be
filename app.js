77cd8aec31107bcdfa132ea87067f9798b7064d2
NaufalRafif25 <72695653+NaufalRafif25@users.noreply.github.com>
2021-02-20 21:01:42 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// console.log("Hello Wolrd");
+
+// let a = 5;
+// let b = 10;
+// let result = a + b;
+
+// console.log(result);
+
+// function sum(angka1, angka2) {
+//     return angka1 + angka2;
+// }
+// console.log(sum(10, 10));
+
+const superheroes = require('superheroes');
+
+console.log(superheroes.random());
+
+for(let i = 0; i < 10; i++) {
+    console.log(superheroes.random())
+}
